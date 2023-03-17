dd011c4a69abf8e204a7a0e957bd43d783acd01a
Boon <vipguest246@gmail.com>
2023-03-17 12:59:25 -0400

    delete some stuff

--- tgui/packages/tgui/interfaces/SocialRating.js
+++ tgui/packages/tgui/interfaces/SocialRating.js
@@ -1,18 +1,10 @@
-import { Section, Table } from '../components';
+import { Section } from '../components';
 import { NtosWindow } from '../layouts';
 import { useBackend } from '../backend';
-import { map } from 'common/collections';
 
 export const SocialRating = (prop, context) => {
   const { act, data } = useBackend(context);
   const { Crewlist = {} } = data;
-  const modified = map((name, number) => (
-    <Table.Row key={name}>
-      <Table.Cell key={name}>
-        {name} {number}
-      </Table.Cell>
-    </Table.Row>
-  ));
   return (
     <NtosWindow width={600} height={500}>
       <NtosWindow.Content scrollable>
